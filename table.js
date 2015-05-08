5f5462cba90451b88b12dee51838d3ef412b30c1
dakaringer <dakaringer@gmail.com>
2015-05-08 13:34:45 -0400

    fix baseurl in js

--- table.js
+++ table.js
@@ -1,5 +1,5 @@
 function populate (year, tt) {
-    $.getJSON("/timetable/" + tt + ".json", function(json) {
+    $.getJSON("/UofT-ExamMap/timetable/" + tt + ".json", function(json) {
         var r = new Array();
         var j = -1;
         $.each(json, function(key, val) {
